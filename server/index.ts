2506c9c76dfc6960c552d4f9ca5e23a2bfc43afc
ericyang1217 <tom200827@gmail.com>
2026-01-27 19:40:55 +0800

    server 設定

--- server/index.ts
+++ server/index.ts
@@ -81,15 +81,16 @@ app.use((req, res, next) => {
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
+  // Other ports are firewalled. Default to 3304 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
+  const port = parseInt(process.env.PORT || "3304", 10);
   httpServer.listen(
     {
       port,
       host: "0.0.0.0",
-      reusePort: true,
+      // reusePort is not supported on Windows.
+      ...(process.platform !== "win32" ? { reusePort: true } : {}),
     },
     () => {
       log(`serving on port ${port}`);
